cd8308c5c74f47fbe51479940da25442c0de612f
Mitchell Hollberg <Mitchell.Hollberg@cfga.titan.1sourcing.net>
2020-02-14 16:46:37 -0500

    Initial commit

---
+++ fn.py
@@ -0,0 +1,50 @@
+"""
+fn.py
+Define functions
+"""
+
+def download_wait(path_to_downloads):
+    """
+    From https://stackoverflow.com/questions/34338897/python-selenium-find-out-when-a-download-has-completed
+    """
+    seconds = 0
+    dl_wait = True
+    while dl_wait:
+        time.sleep(1)
+        dl_wait = False
+        for fname in os.listdir(path_to_downloads):
+            if fname.endswith('.crdownload'):
+                dl_wait = True
+        seconds += 1
+    return seconds
+
+
+def find_by_day_elements():
+    """
+    Capture all "headers" for pictures by day
+    Returns dictionary of {day_as_string: select "all" button for that day}
+    """
+    select_all_by_day_buttons = \
+        driver.find_elements_by_xpath("//div[@class='infinite-loader']//div[@class='section-header']/button[@class='count-select']")
+
+    # If above returns an empty list, scroll/page down and try again until reach the bottom of the page
+    # This ensures cases where a day's photos expand beyond full frame of the window, and thus the next
+    # day's photo's are not yet loaded in the DOM
+    while len(select_all_by_day_buttons) == 0:
+
+        # Quit when you get to bottom of window
+        window_position_before = int(driver.execute_script("return window.pageYOffset;"))
+        # if window_position == window_height:
+        #   exit
+        # Scroll down 1 page
+        html = driver.find_element_by_tag_name('html')
+        html.send_keys(Keys.END)
+        time.sleep(.5)
+        window_position_after = int(driver.execute_script("return window.pageYOffset;"))
+        if window_position_after == window_position_before: # Were already at bottom of page
+            break
+
+        select_all_by_day_buttons = \
+            driver.find_elements_by_xpath("//div[@class='infinite-loader']//div[@class='section-header']/button[@class='count-select']")
+
+    return select_all_by_day_buttons
---
+++ get_photos.py
@@ -0,0 +1,114 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+# from bs4 import BeautifulSoup
+# import re
+# import pandas as pd
+import os
+import time
+import fn
+
+
+"""
+Could try to navigate thusly:
+https://www.amazon.com/photos/all?lcf=time&timeYear=2019&timeMonth=3
+https://www.amazon.com/photos/all?lcf=time&timeYear={year}&timeMonth=3{month_num}
+"""
+
+base_url = f'https://www.amazon.com/photos/all'
+
+save_path = r'C:\Users\mitchell.hollberg\Downloads\AmazonPhotos'
+
+chromeOptions = Options()
+chromeOptions.add_argument('disable_infobars')
+chromeOptions.add_experimental_option('prefs', {'download.default_directory': save_path})
+driver = webdriver.Chrome(chrome_options=chromeOptions)
+
+# 1) Navigate to Amazon Photos Home Page
+driver.get(base_url)
+
+# login logic here...
+driver.find_element_by_id('ap_email').send_keys('myemail@mail.com')
+driver.find_element_by_id('ap_password').send_keys('Mypa33wr!d')
+driver.find_element_by_id('signInSubmit').click()
+time.sleep(2)
+
+# driver.find_elements_by_xpath("//span[text()='Date Taken']")
+
+#  Expand to view all years under "Date Taken" Filter in left navbar
+driver.find_elements_by_xpath\
+    ("//div[@name='year-month-filter']/div"
+     "/button[@class='show-more-button']")[0].click()
+
+# Get all "Year" Buttons, loop over each
+year_buttons = driver.find_elements_by_xpath("//div[@name='years']/ul/ul/li/button")
+for year_button in year_buttons:
+    current_year = year_button.text[:4] # Left 4 characters of YYYY### where ### is # of photos
+    number_of_photos = year_button.text[4:]
+    # Find button location - scroll to that
+    button_x, button_y = year_button.location['x'], year_button.location['y']
+    driver.execute_script(f'window.scrollTo({button_x}, {button_y - 125})')
+    year_button.click()     # Will expand to display year by month taken
+
+    time.sleep(.5)
+
+    # Get all "by month" buttons.
+    month_buttons = driver.find_elements_by_xpath("//div[@name='years']/ul/ul/li/ul/li/button")
+    for month_button in month_buttons:
+        # Find button location - scroll to that
+        button_x, button_y = month_button.location['x'], month_button.location['y']
+        driver.execute_script(f'window.scrollTo({button_x}, {button_y - 125})')
+        current_month = month_button.text[:4]
+        month_button.click()
+        time.sleep(.5)
+
+        days_list = fn.find_by_day_elements()
+
+        time.sleep(.5)
+
+        # Returns the buttons to click
+        for day in days_list:
+
+            # Find button location - scroll to that
+            button_x, button_y = day.location['x'], day.location['y']
+            driver.execute_script(f'window.scrollTo({button_x}, {button_y - 125})')
+
+            time.sleep(.5)
+            day.click()
+
+            # Now that we've scrolled, new "by day" rows may appear in the DOM, so check again
+            current_days_list = fn.find_by_day_elements()
+            for current_day in current_days_list:
+                if current_day in days_list:
+                    continue
+                else:
+                    days_list.append(current_day)
+
+
+        time.sleep(2)
+        # Now all are selected. Click "Download" button
+        try:
+            download_button = driver.find_element_by_class_name('download')
+        except:
+            # Couldn't Find download button - stop and find it manually
+            time.sleep(2)
+            download_button = driver.find_element_by_class_name('download')
+
+        # Find button location - scroll to that
+        button_x, button_y = download_button.location['x'], download_button.location['y']
+        driver.execute_script(f'window.scrollTo({button_x}, {button_y - 125})')
+        download_button.click()
+        time.sleep(.5)
+
+        # Check download directory for the new file. When it appears, rename by year_month, and continue
+        default_filename = f'AmazonPhotos.zip'
+        new_filename = f'AmazonPhotos_{current_year}_{current_month}.zip'
+
+        download_time = fn.download_wait(save_path)
+        time.sleep(1)
+
+        # Rename "AmazonPhotos.zip" file
+        os.rename(f'{save_path}\{default_filename}', f'{save_path}\{new_filename}')
+        print(f'Downloaded Photos from {current_month}, {current_year}.')
+
+moo = 'foo'
